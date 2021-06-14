a90991b98b78496d781308df54c51aee541e7f5f
nataschazw <nataschazwolsman@hotmail.com>
2021-06-14 19:39:03 +0200

    changed userProfile

--- routes/createUserProfile.js
+++ routes/createUserProfile.js
@@ -3,11 +3,32 @@ const router = express.Router();
 
 router.get('/createUserProfile', async (req, res) => {
     try {
-      res.render('addUserProfile');
+      res.render('createUserProfile');
     } catch (error) {
       console.log(error);
     }
   });
+
+router.post("/createUserProfile", async (req, res) => {
+    try {
+    // Get the database conncection
+    database = req.app.get('database');
+    let userProfile = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
+    const result = await database.collection('users').insertOne(userProfile);
+    console.log(userProfile);
+    let userprofileID = userProfile._id;
+    console.log(userprofileID)
+} catch (error) {
+    console.error(error);
+
+  } finally {
+    res.render('createUserProfile');
+  }
+});
 
 // Export the router
 module.exports = router;
