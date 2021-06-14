ae4a5e936f6cdd79ae0be090a339f4edd98cb15f
nataschazw <nataschazwolsman@hotmail.com>
2021-06-14 19:37:26 +0200

    changed dogProfile route

--- routes/createDogProfile.js
+++ routes/createDogProfile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 router.get('/createDogProfile', async (req, res) => {
     try {
@@ -8,6 +9,29 @@ router.get('/createDogProfile', async (req, res) => {
       console.log(error);
     }
   });
+
+  router.post("/createDogProfile", async (req, res) => {
+    try {
+    // Get the database conncection
+    database = req.app.get('database');
+    let dogProfile = {
+        name: req.body.name,
+        age: req.body.age,
+        breed: req.body.breed,
+        description: req.body.description,
+        vibe: req.body.vibe,
+        location: req.body.location
+    };
+    const result = await database.collection('doggos').insertOne(dogProfile);
+    console.log(dogProfile);
+
+} catch (error) {
+    console.error(error);
+
+  } finally {
+    res.render('createDogProfile');
+  }
+});
 
 // Export the router
 module.exports = router;
