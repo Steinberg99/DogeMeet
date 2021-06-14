e65dee0f0a506f91692f89b0f2f674d42a82db1a
nataschazw <nataschazwolsman@hotmail.com>
2021-06-14 20:06:46 +0200

    added nodemailer

---
+++ routes/mail.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+router.get('/mail', async (req, res) => {
+    try {
+      res.render('mail');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  router.post('/mail', async (req, res) => {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'dogemeetapp@gmail.com',
+            pass: 'doggo123.'
+        }
+          });
+        
+          let mailOptions = {
+              from: '"DogeMeet team" <dogemeetapp@gmail.com>', //sender
+              to: 'natascha.zwolsman@hva.nl', // receiver
+              subject: 'Welcome to Dogemeet', //subject line
+              text: 'hello', //plain text body
+              html: '<h1>hello</h1>' // vervangen met tekst
+          };
+        
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              } else {
+              console.log("Message sent: %s", info.messageId);
+              console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+              res.render('mail');
+            }
+          });
+   
+  });
+
+// Export the router
+module.exports = router;
